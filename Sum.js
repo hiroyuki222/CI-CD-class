01935734628603b299a048a56e20db63a8a9513c
Danilo Sanada <danilohsanada@hotmail.com>
2025-08-14 23:06:14 -0300

    add test

--- Sum.js
+++
@@ -1,9 +0,0 @@
-function main(a, b){
-    return Sum(a, b);
-}
-
-function Sum(a, b){
-    return a + b;
-}
-
-console.log(main(1, 2));
---
+++ src/sum.js
@@ -0,0 +1,9 @@
+function main(a, b) {
+    return Sum(a, b);
+}
+
+function Sum(a, b) {
+    return a + b;
+}
+
+module.exports = main;
