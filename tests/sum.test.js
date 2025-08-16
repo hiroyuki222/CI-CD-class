d925074b5accfd6a22eebe2ee38e6b4c8acf5c3c
Danilo Sanada <danilohsanada@hotmail.com>
2025-08-16 01:04:50 -0300

    add sonar

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,9 @@
-const soma = require('../src/sum');
+const main = require('../src/Sum');
 
 test('soma 2 + 3 = 5', () => {
-  expect(soma(2, 3)).toBe(5);
+  expect(main(2, 3)).toBe(5);
+});
+
+test('exporta uma função', () => {
+  expect(typeof main).toBe('function');
 });
